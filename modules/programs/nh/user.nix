d1ff4e76c695e3d7c8ddc603d68039e774f02b30
senyubia <108198161+senyubia@users.noreply.github.com>
2026-04-09 18:43:39 +0200

    conf(nh): move nh to user and remove clean service

--- modules/programs/nh/user.nix
+++ modules/programs/nh/user.nix
@@ -1,12 +1,10 @@
 { user, pkgs, ... }: {
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep 5";
     flake = "/home/${user.name}/nix";
   };
 
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     (writeShellScriptBin "ns" ''
       if [ $# -eq 0 ]; then
         echo "Usage: ns <package1> <package2> ..."
