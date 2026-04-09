ad4a56202a809350d7152c76f466d7760ef5867b
senyubia <108198161+senyubia@users.noreply.github.com>
2026-04-09 19:15:45 +0200

    chore(network): remove comments

--- modules/system/network/system.nix
+++ modules/system/network/system.nix
@@ -1,10 +1,3 @@
 {
   networking.networkmanager.enable = true;
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
 }
