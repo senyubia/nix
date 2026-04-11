d085443f8a3aa5fbc94686e3f5ed05a28dc3a56d
senyubia <108198161+senyubia@users.noreply.github.com>
2026-04-11 16:03:33 +0200

    conf: install packages to system

---
+++ modules/programs/nh/system.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    nh
+
+    (writeShellScriptBin "ns" ''
+      if [ $# -eq 0 ]; then
+        echo "Usage: ns <package1> <package2> ..."
+        exit 1
+      fi
+
+      for arg do
+        set -- "$@" "nixpkgs#$arg"
+        shift
+      done
+
+      exec nix shell "$@"
+    '')
+  ];
+}
--- modules/programs/nh/user.nix
+++ modules/programs/nh/user.nix
@@ -3,20 +3,4 @@
     enable = true;
     flake = "/home/${user.name}/nix";
   };
-
-  home.packages = with pkgs; [
-    (writeShellScriptBin "ns" ''
-      if [ $# -eq 0 ]; then
-        echo "Usage: ns <package1> <package2> ..."
-        exit 1
-      fi
-
-      for arg do
-        set -- "$@" "nixpkgs#$arg"
-        shift
-      done
-
-      exec nix shell "$@"
-    '')
-  ];
 }
