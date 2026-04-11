3fe88d7f3911a23d1a323b0732d082e917f0f79f
senyubia <108198161+senyubia@users.noreply.github.com>
2026-04-11 15:38:44 +0200

    chore(laptop/hardware): refactor hardware

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -1,9 +1,7 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
-{
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+{ config, lib, modulesPath, ... }: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
