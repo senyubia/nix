de3d76dd1e11bf2febe132dee9ea0a50f02351be
senyubia <108198161+senyubia@users.noreply.github.com>
2026-04-20 15:42:08 +0200

    conf(hardware): switch to zen kernel

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -1,8 +1,9 @@
-{ config, lib, modulesPath, ... }: {
+{ config, pkgs, lib, modulesPath, ... }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
