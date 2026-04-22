938edb779b8f6d3febbf501776d37ade5ea2a819
senyubia <108198161+senyubia@users.noreply.github.com>
2026-04-22 02:11:19 +0200

    chore(flake): move flake outputs into a separate file

---
+++ outputs.nix
@@ -0,0 +1,80 @@
+{ nixpkgs, nixpkgs-stable, disko, home-manager, ...} @ inputs: let
+  config = import ./config.nix;
+  moduleImporter = import ./lib/moduleImporter.nix { root = ./.; };
+
+  assets = ./assets;
+  inherit (import "${config.selectedHost}/info.nix") host user;
+
+  hostModules = import "${config.selectedHost}/modules.nix" { modules = moduleImporter.modules; };
+  systemModules = builtins.filter (m: m != null) (map (m: m.system or null) hostModules);
+  userModules = builtins.filter (m: m != null) (map (m: m.user or null) hostModules);
+
+in {
+  nixosConfigurations.${host.name} = nixpkgs.lib.nixosSystem {
+    system = host.arch;
+
+    specialArgs = {
+      inherit inputs host user assets;
+      modules = moduleImporter.modules;
+    };
+
+    modules = systemModules ++ [
+      {
+        nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+        nixpkgs = {
+          config.allowUnfree = true;
+
+          overlays = [
+            (final: prev: {
+              stable = import nixpkgs-stable {
+                system = host.arch;
+                config.allowUnfree = true;
+              };
+            })
+          ];
+        };
+
+        networking.hostName = host.name;
+
+        users.users.${user.name} = {
+          isNormalUser = true;
+          description = user.fullName;
+          extraGroups = [ "networkmanager" "wheel" ];
+        };
+
+        system.stateVersion = host.state;
+      }
+
+      "${config.selectedHost}/hardware.nix"
+      "${config.selectedHost}/packages.nix"
+
+      disko.nixosModules.disko
+      "${config.selectedHost}/disk.nix"
+
+      home-manager.nixosModules.home-manager
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          backupFileExtension = "backup";
+
+          extraSpecialArgs = {
+            inherit inputs host user assets;
+            modules = moduleImporter.modules;
+          };
+
+          users.${user.name}.imports = userModules ++ [
+            {
+              home = {
+                username = user.name;
+                homeDirectory = "/home/${user.name}";
+                stateVersion = host.state;
+              };
+            }
+          ];
+        };
+      }
+    ];
+  };
+}
