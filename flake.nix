bfccd9228d76b3cf2b86977ae06c258e27b1baa6
senyubia <108198161+senyubia@users.noreply.github.com>
2026-06-14 18:01:23 +0200

    chore(flake): update system

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
