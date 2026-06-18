fe20762ee27ce1d65ca94a68384ed333c007abae
senyubia <108198161+senyubia@users.noreply.github.com>
2026-06-18 23:31:26 +0200

    conf(audio): revert previous changes

--- modules/system/audio/default.nix
+++ modules/system/audio/default.nix
@@ -5,26 +5,6 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-
-      extraConfig.pipewire = {
-        "92-low-latency" = {
-          "context.properties" = {
-            "default.clock.rate" = 48000;
-            "default.clock.quantum" = 128;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 512;
-          };
-        };
-      };
-
-      wireplumber.extraConfig = {
-        "10-bluetooth-policy" = {
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true; 
-            "bluez5.default.rate" = 48000;
-          };
-        };
-      };
     };
 
     security.rtkit.enable = true;
