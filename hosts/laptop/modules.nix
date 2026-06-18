63c04933ebee1cd71d4ff28405236fc99bfd49ec
senyubia <108198161+senyubia@users.noreply.github.com>
2026-06-18 10:53:53 +0200

    chore(laptop/modules): reorder laptop modules

--- hosts/laptop/modules.nix
+++ hosts/laptop/modules.nix
@@ -1,8 +1,10 @@
 { modules }: [
+  # bundles
   modules.bundle.base.system
   modules.bundle.base.program.cli
   modules.bundle.base.program.gui
 
+  # system
   modules.system.bootloader.grub
   modules.system.gpu.nvidia_optimus
   modules.system.bluetooth
@@ -10,14 +12,16 @@
   modules.system.ssh
   modules.system.elevation.sudo
 
+  # ui
   modules.graphical.displaymanager.ly
   modules.graphical.desktop.hyprland.nvidia_optimus
+  modules.config.stylix
 
+  # base apps
   modules.system.shell.zsh
   modules.program.cli.editor.micro
   modules.program.cli.yazi
 
-  modules.config.stylix
-
+  # extra
   modules.program.gui.gaming
 ]
